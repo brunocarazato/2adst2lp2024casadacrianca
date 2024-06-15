f7d6e1b45a8911b9d5aab1d17e607a6c4ed93200
Bruno Carazato <brunocarazatoo@gmail.com>
2024-06-14 21:52:38 -0300

    script.sql

---
+++ casa_da_crianca.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE `casa_da_crianca` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+
+USE `casa_da_crianca`;
+
+CREATE TABLE `crianca` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(35) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
